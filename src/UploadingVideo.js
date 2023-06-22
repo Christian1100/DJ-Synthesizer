56e2c5211958cdb8f626d88a186ca984a685e5f0
Christian Kaltenbach <christian123kalt@gmail.com>
2023-06-22 17:30:10 +0200

    adding file selection, dragover and drop events for video-upload

--- src/UploadingVideo.js
+++ src/UploadingVideo.js
@@ -1,16 +1,41 @@
+import { useRef } from "react";
 import { useState } from "react";
 
 const UploadingVideo = () => {
     const [files, setFiles] = useState(null);
+    const inputRef = useRef();
+
+    const dragOverManager = (event) => {
+        event.preventDefault();
+    };
+
+    const dropManager = (event) => {
+        event.preventDefault();
+        setFiles(event.dataTransfer.files)
+    };
+
+    if (files) return (
+        <div className="uploads">
+            <ul>
+                {Array.from(files).map((file, idx) => <li key={idx}>{file.name}</li>)}
+            </ul>
+        </div>
+    )
 
     return (
         <>
-            {!files && (
-                <div className="dropzone">
+            <div className="dropzone" onDragOver={dragOverManager} onDrop={dropManager}>
                     <h2>Drag and Drop Video to Upload</h2>
-                    <button>Select Video</button>
+
+                    <input 
+                        type="file" 
+                        onChange={(event) => setFiles(event.target.files)} 
+                        hidden 
+                        ref={inputRef}
+                        accept="video/*">
+                    </input>
+                    <button onClick={() => inputRef.current.click()}>Select Video</button>
                 </div>
-            )}
         </>
     )
 };

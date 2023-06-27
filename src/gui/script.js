fdcaf4cef425e32096de870665e411279b27d046
luayaljumaa <luay.aljumaa@gmail.com>
2023-06-27 18:09:57 +0200

    hier ist der neue Version von der Oberfläche. die Knopfe können sich mit dem Klick des Mouse bewegen und in beide Richtungen bewegt werden. die kleine Knpfe Oben bewegen sich einzel mit einem Klick und nicht in beide Richtungen

--- src/gui/script.js
+++ src/gui/script.js
@@ -12,9 +12,38 @@ turntables.forEach(function(turntable, index) {
 });
 
 
-var playPauseButton = document.getElementById('play-pause');
-var triangle = document.getElementById('triangle');
+var turntables = document.querySelectorAll('[id^="turntable"]');
+var rotations = [];
 
-playPauseButton.addEventListener('click', function() {
-    triangle.classList.toggle('paused');
+turntables.forEach(function(turntable, index) {
+    rotations[index] = 0;
+    var isMouseDown = false;
+
+    turntable.addEventListener('mousedown', function() {
+        isMouseDown = true;
+    });
+
+    turntable.addEventListener('mouseup', function() {
+        isMouseDown = false;
+    });
+
+    turntable.addEventListener('mousemove', function(event) {
+        if (isMouseDown) {
+
+            // Die Mausposition relativ zum Drehknopf erhalten
+            var knobRect = turntable.querySelector('#knob-big').getBoundingClientRect();
+            var mouseX = event.clientX - knobRect.left;
+            var mouseY = event.clientY - knobRect.top;
+
+            // Den Winkel berechnen
+            var angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+            rotations[index] = angle;
+
+            // Die Linie drehen
+            turntable.querySelector('#line-big').style.transform = 'rotate(' + rotations[index] + 'deg)';
+        }
+    });
 });
+
+
+

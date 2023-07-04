12b821642fd88026f281c2b1274cfb5d6f37d161
luayaljumaa <luay.aljumaa@gmail.com>
2023-07-04 15:31:45 +0200

    hier ist der neue Version von der Oberfläche. die Knopfe können sich mit dem Klick des Mouse bewegen und in beide Richtungen bewegt werden. die kleine Knpfe Oben bewegen sich einzel mit einem Klick und nicht in beide Richtungen. equalizer habe ich weg gelassen aber kann ich wieder bei bedarf implementieren :)

--- src/gui/script.js
+++ src/gui/script.js
@@ -31,7 +31,7 @@ turntables.forEach(function(turntable, index) {
         if (isMouseDown) {
 
             // Die Mausposition relativ zum Drehknopf erhalten
-            var knobRect = turntable.querySelector('#knob-big').getBoundingClientRect();
+            var knobRect = turntable.querySelector('#knob-mp-low').getBoundingClientRect();
             var mouseX = event.clientX - knobRect.left;
             var mouseY = event.clientY - knobRect.top;
 
@@ -40,10 +40,75 @@ turntables.forEach(function(turntable, index) {
             rotations[index] = angle;
 
             // Die Linie drehen
-            turntable.querySelector('#line-big').style.transform = 'rotate(' + rotations[index] + 'deg)';
+            turntable.querySelector('#line-mp-low').style.transform = 'rotate(' + rotations[index] + 'deg)';
+        }
+    });
+    turntable.addEventListener('mousemove', function(event) {
+        if (isMouseDown) {
+
+            // Die Mausposition relativ zum Drehknopf erhalten
+            var knobRect = turntable.querySelector('#knob-mp-middle').getBoundingClientRect();
+            var mouseX = event.clientX - knobRect.left;
+            var mouseY = event.clientY - knobRect.top;
+
+            // Den Winkel berechnen
+            var angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+            rotations[index] = angle;
+
+            // Die Linie drehen
+            turntable.querySelector('#line-mp-middle').style.transform = 'rotate(' + rotations[index] + 'deg)';
+        }
+    });
+    turntable.addEventListener('mousemove', function(event) {
+        if (isMouseDown) {
+
+            // Die Mausposition relativ zum Drehknopf erhalten
+            var knobRect = turntable.querySelector('#knob-mp-high').getBoundingClientRect();
+            var mouseX = event.clientX - knobRect.left;
+            var mouseY = event.clientY - knobRect.top;
+
+            // Den Winkel berechnen
+            var angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+            rotations[index] = angle;
+
+            // Die Linie drehen
+            turntable.querySelector('#line-mp-high').style.transform = 'rotate(' + rotations[index] + 'deg)';
+        }
+    });
+    turntable.addEventListener('mousemove', function(event) {
+        if (isMouseDown) {
+
+            // Die Mausposition relativ zum Drehknopf erhalten
+            var knobRect = turntable.querySelector('#knob-high').getBoundingClientRect();
+            var mouseX = event.clientX - knobRect.left;
+            var mouseY = event.clientY - knobRect.top;
+
+            // Den Winkel berechnen
+            var angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+            rotations[index] = angle;
+
+            // Die Linie drehen
+            turntable.querySelector('#line-high').style.transform = 'rotate(' + rotations[index] + 'deg)';
+        }
+    });
+    turntable.addEventListener('mousemove', function(event) {
+        if (isMouseDown) {
+
+            // Die Mausposition relativ zum Drehknopf erhalten
+            var knobRect = turntable.querySelector('#knob-low-1').getBoundingClientRect();
+            var mouseX = event.clientX - knobRect.left;
+            var mouseY = event.clientY - knobRect.top;
+
+            // Den Winkel berechnen
+            var angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+            rotations[index] = angle;
+
+            // Die Linie drehen
+            turntable.querySelector('#line-low-1').style.transform = 'rotate(' + rotations[index] + 'deg)';
         }
     });
 });
+
 
 
 

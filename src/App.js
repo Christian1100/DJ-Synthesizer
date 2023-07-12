22f2892ffa19986ff4bca3037e018777bfbe9912
David Wolff <david@dav.dev>
2023-07-12 23:53:14 +0200

    Fix dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import UploadingVideo from "./UploadingVideo";
 import AudioTestButton from './AudioTestButton';
+import Equalizer from './gui/Equalizer/Equalizer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <AudioTestButton />
+        <Equalizer />
         <UploadingVideo />
       </header>
     </div>
